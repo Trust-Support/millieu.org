71e34d191af4efcc68f7fd739d4112a558f080cc
pwrstudio <pwrpntr@gmail.com>
2021-03-29 15:59:52 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<h1>🌳 Milieu.org</h1>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 4em;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
